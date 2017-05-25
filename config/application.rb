b35b15f6abe1bbdf5963b12d864c720d22e5fc32
migvasc <miguel.vasconcelos@usp.br>
2017-05-25 00:00:53 +0000

    travis fix

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
+require 'devise'
 Bundler.require(*Rails.groups)
 
 module Workspace
