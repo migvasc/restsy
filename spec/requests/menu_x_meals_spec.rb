8a1e09466fbc52e70bc448f810d829939345865f
migvasc <miguel.vasconcelos@usp.br>
2017-06-18 00:29:52 +0000

    tentando resolver testes travis 2

--- spec/requests/menu_x_meals_spec.rb
+++ spec/requests/menu_x_meals_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "MenuXMeals", type: :request do
 #  describe "GET /menu_x_meals" do
   # # it "works! (now write some real specs)" do
-      get menu_x_meals_path
+    #  get menu_x_meals_path
   #    expect(response).to have_http_status(200)
    # end
  # end
