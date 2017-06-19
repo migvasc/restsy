a117f149aac85e0b062cc46292b4cf5ee03c1772
migvasc <miguel.vasconcelos@usp.br>
2017-06-19 00:10:43 +0000

    recuperando os dados

--- db/migrate/20170618195224_remove_email_from_customers.rb
+++
@@ -1,5 +0,0 @@
-class RemoveEmailFromCustomers < ActiveRecord::Migration
-  def change
-    remove_column :customers, :email, :string
-  end
-end
--- db/migrate/20170618195234_remove_password_from_customers.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordFromCustomers < ActiveRecord::Migration
-  def change
-    remove_column :customers, :password, :string
-  end
-end
