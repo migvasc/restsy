ee264b30f9fad5fc38bb3172a797b7a9448ec932
migvasc <miguel.vasconcelos@usp.br>
2017-06-29 00:28:48 +0000

    Corrigindo Spec 4

--- spec/requests/my_lists_spec.rb
+++ spec/requests/my_lists_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "MyLists", type: :request do
  # describe "GET /my_lists" do
   #  it "works! (now write some real specs)" do
    #   get my_lists_path
-      expect(response).to have_http_status(200)
+   #   expect(response).to have_http_status(200)
   #  end
 #  end
 end
